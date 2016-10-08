fc37ca59dba1124345393deeaea91122bd05c955
Isaiah Mann <contact@isaiahmann.com>
2016-10-08 12:49:26 -0400

    Pushing the beginnings of node connections

---
+++ Assets/Scripts/World/Connection.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Connection : StaticObjectBehaviour {
+	[SerializeField]
+	Node startNode;
+
+	[SerializeField]
+	Node endNode;
+
+	LineRenderer visualLine;
+
+	public void InitializeConnection (Node node) {
+		this.startNode = node;
+	}
+
+	public void CompleteConnection (Node node) {
+		this.endNode = node;
+	}
+
+	protected override void SetReferences () {
+		base.SetReferences ();
+		visualLine = GetComponent<LineRenderer>();
+	}
+
+	void setStartPosition (Vector3 worldPosition) {
+
+	}
+
+	void setEndPosition (Vector3 worldPosition) {
+
+	}
+}
--- Assets/Scripts/World/Player.cs
+++ Assets/Scripts/World/Player.cs
@@ -7,5 +7,16 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : Agent {
-	
+	public static Player Instance;
+
+	protected override void SetReferences () {
+		if (SingletonUtil.TryInit(ref Instance, this, gameObject)) {
+			base.SetReferences ();	
+		}
+	}
+
+	protected override void CleanupReferences () {
+		base.CleanupReferences ();
+		SingletonUtil.TryCleanupSingleton(ref Instance, this);
+	}
 }
