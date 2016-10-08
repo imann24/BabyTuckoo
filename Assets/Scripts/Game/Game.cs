95451288c9f74c15507f997f4bc0ed3c0537fc04
Isaiah Mann <contact@isaiahmann.com>
2016-10-08 14:37:42 -0400

    Adding home node (will fix out of grid error later Joseph)

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Game : MannBehaviour {
+	public GameObject[] AgentPrefabs;
+	Agent[] agents;
+	public GameObject GridPrefab;
+	Grid currentGrid;
+
+	protected override void CleanupReferences () {
+		// Nothing
+	}
+
+	protected override void FetchReferences () {
+		// Nothing
+	}
+
+	protected override void HandleNamedEvent (string eventName) {
+		// Nothing
+	}
+
+	protected override void SetReferences () {
+		createAgents();
+		createGrid();
+		currentGrid.AddHomeNodes(agents);
+	}
+
+	void createAgents () {
+		agents = new Agent[AgentPrefabs.Length];
+		int index = 0;
+		foreach (GameObject agentPrefab in AgentPrefabs) {
+			agents[index++] = Instantiate(agentPrefab).GetComponent<Agent>();
+		}
+	}
+
+	void teardownAgents () {
+		if (agents != null) {
+			for (int i = 0; i < agents.Length; i++) {
+				Destroy(agents[i].gameObject);
+			}
+			agents = new Agent[0];
+		}
+	}
+
+	void createGrid () {
+		currentGrid = Instantiate(GridPrefab).GetComponent<Grid>();
+	}
+
+	void teardownGrid () {
+		if (currentGrid) {
+			Destroy(currentGrid);
+		}
+	}
+}
--- Assets/Scripts/World/HomeNode.cs
+++ Assets/Scripts/World/HomeNode.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class HomeNode : Node {
 	public void Capture (Agent agent) {
-
+		agent.Win();
 	}
 }
