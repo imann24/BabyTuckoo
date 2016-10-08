486fe0b9d7efaeffb9c7582e32c45c8f1cec4e6d
Joseph Gillen <joegillen@ymail.com>
2016-10-08 14:05:16 -0400

    Create enemy singleton prefab

--- Assets/Scripts/World/Enemy.cs
+++ Assets/Scripts/World/Enemy.cs
@@ -7,5 +7,19 @@ using UnityEngine;
 using System.Collections;
 
 public class Enemy : AIAgent {
+    public static Enemy Instance;
 
+    protected override void SetReferences()
+    {
+        if (SingletonUtil.TryInit(ref Instance, this, gameObject))
+        {
+            base.SetReferences();
+        }
+    }
+
+    protected override void CleanupReferences()
+    {
+        base.CleanupReferences();
+        SingletonUtil.TryCleanupSingleton(ref Instance, this);
+    }
 }
