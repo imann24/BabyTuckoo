56d22232581290b1bce9e9ffcb78da088dcb5bbc
Isaiah Mann <contact@isaiahmann.com>
2016-10-09 11:52:23 -0400

    Beginning ai logic

--- Assets/Scripts/World/AIAgent.cs
+++ Assets/Scripts/World/AIAgent.cs
@@ -6,7 +6,55 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class AIAgent : Agent {
+	public float DecisionTime = 2.0f;
+	Queue<MannAction> futureActions = new Queue<MannAction>();
+	bool _isActive = true;
+	public bool IsActive {
+		get {
+			return _isActive;
+		}
+		set {
+			_isActive = value;
+		}
+	}
+	IEnumerator decisionCoroutine;
+	IEnumerator executeCoroutine;
 
+	protected override void SetReferences () {
+		base.SetReferences ();
+		startDecisionLoop();
+	}
+
+	protected void startDecisionLoop () {
+		haltDecisionLoop();
+		decisionCoroutine = decisionLoop(DecisionTime);
+		StartCoroutine(decisionCoroutine);
+	}
+
+	protected void haltDecisionLoop () {
+		if (decisionCoroutine != null) {
+			StopCoroutine(decisionCoroutine);
+		}
+	}
+
+	IEnumerator decisionLoop (float decisionTime) {
+		while (IsActive) {
+			futureActions.Enqueue(decideNextAction());
+			yield return new WaitForSeconds(decisionTime);
+		}
+	}
+
+	protected virtual MannAction decideNextAction () {
+		return delegate {
+			Debug.Log("Running");
+		};
+	}
+
+	protected virtual void executeNextAction () {
+		futureActions.Dequeue()();
+	}
 }
